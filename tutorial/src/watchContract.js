017ac1def067bcf00804dd62eaf4fbd688e6ae5e
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-01-20 01:09:37 -0500

    has invalid network error

--- tutorial/src/watchContract.js
+++ tutorial/src/watchContract.js
@@ -1,14 +1,12 @@
-import App from './App.jsx'
-
 
 const ethers = require('ethers')
 
-const abi = require('../EtherRide/foo/build/contracts/CounterApp.json').abi
+const abi = require('../../../EtherRide/foo/build/contracts/CounterApp.json').abi
 
 let provider = new ethers.providers.JsonRpcProvider('http://localhost:8545', 'undefined')
 
 let contract = new ethers.Contract('0xc1f4085e8a1eccdcf198f40762469e6bbb4dae52', abi, provider)
 
-contract.on('UnlockCar', () => { App.unlock() })
+contract.on('UnlockCar', () => { return axios.post(`http://localhost:8000/lock`); })
 
-contract.on('LockCar', () => { App.lock() })
+contract.on('LockCar', () => { return axios.post(`http://localhost:8000/unlock`); })
