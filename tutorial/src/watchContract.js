5ea582322f2d837b2f821427433b9d27531b129c
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-01-20 01:11:28 -0500

    changed lock/unlock error

--- tutorial/src/watchContract.js
+++ tutorial/src/watchContract.js
@@ -7,6 +7,6 @@ let provider = new ethers.providers.JsonRpcProvider('http://localhost:8545', 'un
 
 let contract = new ethers.Contract('0xc1f4085e8a1eccdcf198f40762469e6bbb4dae52', abi, provider)
 
-contract.on('UnlockCar', () => { return axios.post(`http://localhost:8000/lock`); })
+contract.on('UnlockCar', () => { return axios.post(`http://localhost:8000/unlock`); })
 
-contract.on('LockCar', () => { return axios.post(`http://localhost:8000/unlock`); })
+contract.on('LockCar', () => { return axios.post(`http://localhost:8000/lock`); })
